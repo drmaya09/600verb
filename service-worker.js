f674c2e54f0c80677e13b6bc7de232aaeed96f21
drmaya09 <denizfiratozturk@gmail.com>
2026-08-01 17:58:09 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "fiil-2fc0c3eec0";
+const CACHE = "fiil-2c3e8a59f9";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
